3b2cf095b70357d307ed09fdd15d0c1192699f10
Roman Hadiatskyi <rhadiats@e1r1p3.unit.ua>
2017-05-05 22:06:36 +0300

    moulitest: 5 failed

--- src/ft_s_high.c
+++ src/ft_s_high.c
@@ -17,18 +17,23 @@ int			ft_s_high(va_list elem, t_flags flags)
 	wchar_t		*s;
 	int			i;
 	int			n;
+	int			wise;
 
 	n = 0;
 	i = 0;
 	(void)flags;
+	wise = flags.get_width - flags.get_precision;
 	s = (wchar_t *)va_arg(elem, wchar_t *);
 	if (s == NULL)
 	{
 		flags.args = ft_strdup("(null)");
 		return (ft_printing(flags.args));
 	}
+	if (flags.zero == 1)
+		while (wise-- > 0)
+			n += write(1, "0", 1);
 	if (flags.precision == 1)
-		while(s[i] && i < (flags.get_precision - 1))
+		while(s[i] && i < flags.get_precision)
 			n += choose_mask(s[i++]);
 	else
 		while (s[i])
